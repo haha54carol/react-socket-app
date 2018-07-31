88d82391c6389f13a8f5a4ad1666002edee41664
Carol Ni <carol.ni@sapphireinfo.com.tw>
2018-07-31 17:18:18 +0800

    update client socket

--- client/src/clientSocket.js
+++ client/src/clientSocket.js
@@ -1,44 +1,31 @@
 import io from 'socket.io-client'
 const url = 'http://localhost:4000'
 
-const socketInstance = (path) => {
-    let instance
-
-    if (instance) {
-        return instance
-    }
-
-    instance = io(`${url}${path}`)
-
-    return instance
-
-}
-
 
 let secondsSocket, twoSencondsSocket
 
 const subscribeToSeconds = (fn) => {
     if (!secondsSocket) {
-        secondsSocket = socketInstance('/Seconds')
+        secondsSocket = io(`${url}/Seconds`)
     }
+
     secondsSocket.on('Seconds', sec => fn(sec))
 }
 
 const unsubscribeToSeconds = () => {
-
     secondsSocket.disconnect()
     secondsSocket = null
 }
 
 const subscribeToTwoSeconds = (fn) => {
     if (!twoSencondsSocket) {
-        twoSencondsSocket = socketInstance('/TwoSeconds')
+        twoSencondsSocket = io(`${url}/TwoSeconds`)
     }
+
     twoSencondsSocket.on('TwoSeconds', sec => fn(sec))
 }
 
 const unsubscribeToTwoSeconds = () => {
-
     twoSencondsSocket.disconnect()
     twoSencondsSocket = null
 }
